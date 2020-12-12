1808e5b1b439c30e769d43067bf7ec0bfd5bcdbe
xuzhoukai <xuzhoukai2599@126.com>
2020-12-12 10:27:53 +0800

    测试线程池

--- src/main/java/org/example/theadPool/TheadPoolDemo.java
+++ src/main/java/org/example/theadPool/TheadPoolDemo.java
@@ -1,11 +1,13 @@
 package org.example.theadPool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class TheadPoolDemo {
     public static void main(String[] args) {
-        singleExecutor();
+//        singleExecutor();
+//        fixExecutor();
+//        fixExecutorWithBack();
+        scheduleExcutor();
     }
 
     public static void singleExecutor(){
@@ -14,6 +16,49 @@ public class TheadPoolDemo {
             singleExecutor.execute(new JobOne());
         }
         singleExecutor.shutdown();
+    }
+
+    public static void fixExecutor(){
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        for(int i=0;i<10;i++){
+            executorService.execute(new JobOne());
+        }
+        executorService.shutdown();
+    }
+
+    public static void fixExecutorWithBack(){
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+//        for(int i=0;i<10;i++){
+//            Future<String> submit = executorService.submit(new JobTwo());
+//            try {
+//                String s = submit.get();
+//                System.out.println(i+s);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            } catch (ExecutionException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        for(int i=0;i<10;i++){
+            String result="";
+            Future<String> submit = executorService.submit(new JobOne(),result);
+            try {
+                String s = submit.get();
+                System.out.println(i+s);
+                System.out.println(result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executorService.shutdown();
+    }
+
+    public static void scheduleExcutor(){
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleWithFixedDelay(new JobOne(),0,3,TimeUnit.SECONDS);
+
     }
 }
 
@@ -24,5 +69,15 @@ class JobOne implements Runnable{
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName());
+        return;
+    }
+}
+
+class JobTwo implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        System.out.println(Thread.currentThread().getName());
+        return "hello";
     }
 }
